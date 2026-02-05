dac37d56ab5a613a3e56b564ee536e32b2e7cccc
Yehor Borkov <yehor.borkov@gmail.com>
2026-02-05 17:24:43 +0200

    refactor(nvim): load local plugins using dir and not dev

--- nvim/dot-config/nvim/lua/config/lazy.lua
+++ nvim/dot-config/nvim/lua/config/lazy.lua
@@ -15,9 +15,6 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  dev = {
-    path = vim.fn.stdpath("config") .. "/pack/user/start",
-  },
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
--- nvim/dot-config/nvim/lua/plugins/user.lua
+++ nvim/dot-config/nvim/lua/plugins/user.lua
@@ -1,4 +1,11 @@
+local _dir = vim.fs.joinpath(vim.fn.stdpath("config"), "/pack/user/opt")
+---@param name string
+---@return string
+local dir = function(name)
+  return vim.fs.joinpath(_dir, name)
+end
+
 return {
-  { "user/rust", dev = true },
-  { "user/ukrainian", dev = true, event = "InsertEnter" },
+  { dir = dir("rust") },
+  { dir = dir("ukrainian"), event = "InsertEnter" },
 }
