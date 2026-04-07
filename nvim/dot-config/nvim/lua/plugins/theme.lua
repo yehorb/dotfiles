ea1ae07c04b47652e8b3182da3814e6ac0b66a24
Yehor Borkov <yehor.borkov@gmail.com>
2026-04-07 20:52:35 +0300

    feat(nvim): dofile omarchy theme file

--- nvim/dot-config/nvim/lua/plugins/theme.lua
+++ nvim/dot-config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,7 @@
-return {}
+local current_theme_path = vim.fs.normalize("$HOME/.config/omarchy/current/theme/neovim.lua")
+
+if vim.uv.fs_stat(current_theme_path) then
+  return dofile(current_theme_path)
+else
+  return {}
+end
