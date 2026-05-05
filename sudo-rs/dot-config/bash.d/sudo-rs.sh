1ca0b36a05f69b7561807ad5c02ee798f05061d1
Yehor Borkov <yehor.borkov@gmail.com>
2026-05-05 11:38:37 +0300

    fix(sudo-rs): only create alias if sudo-rs is available

--- sudo-rs/dot-config/bash.d/sudo-rs.sh
+++ sudo-rs/dot-config/bash.d/sudo-rs.sh
@@ -1,4 +1,6 @@
-alias su=su-rs
-alias sudo=sudo-rs
-alias sudoedit=sudoedit-rs
-alias visudo=visudo-rs
+if command -v sudo-rs &>/dev/null; then
+  alias su=su-rs
+  alias sudo=sudo-rs
+  alias sudoedit=sudoedit-rs
+  alias visudo=visudo-rs
+fi
