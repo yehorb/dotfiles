572aa56fcec04fcd0c006a6361fc2c541ee371b0
Yehor Borkov <yehor.borkov@gmail.com>
2026-05-14 22:46:24 +0300

    refactor(hypr): port autostart

--- hypr/dot-config/hypr/autostart.lua
+++ hypr/dot-config/hypr/autostart.lua
@@ -1,2 +1,7 @@
 -- Extra autostart processes.
 -- o.launch_on_start("my-service")
+
+-- Stop locking computer when idle by default
+hl.on("hyprland.start", function()
+	hl.exec_cmd("bash -lc 'sleep 2; pkill -x hypridle; pkill -RTMIN+9 waybar'")
+end)
