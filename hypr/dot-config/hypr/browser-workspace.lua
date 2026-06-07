2748cfa99f5bb04675cdc241f178e5749d6895e1
Yehor Borkov <yehor.borkov@gmail.com>
2026-06-07 15:46:12 +0300

    refactor(hypr): use lua to configure browser window

--- hypr/dot-config/hypr/browser-workspace.lua
+++ hypr/dot-config/hypr/browser-workspace.lua
@@ -1,11 +1,15 @@
+hl.config({
+  scrolling = {
+    explicit_column_widths = "0.49, 0.99",
+    follow_min_visible = 0.0,
+  },
+})
 hl.workspace_rule({ workspace = "1", layout = "scrolling" })
+
 hl.window_rule({
   match = { class = "app.zen_browser.zen" },
   workspace = "1",
+  scrolling_width = 0.99,
 })
-hl.config({
-  scrolling = {
-    explicit_column_widths = "0.5,1.0",
-  },
-})
+
 hl.bind("SUPER + SHIFT + T", hl.dsp.layout("colresize +conf"), { description = "Toggle window column width" })
