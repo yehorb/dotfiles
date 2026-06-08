e595627bdb931bd5b288d9cefa2358ed9f1d4b4f
Yehor Borkov <yehor.borkov@gmail.com>
2026-06-08 19:37:35 +0300

    feat(nvim): set up list

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -2,3 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+vim.o.list = true
+vim.opt.listchars = {
+  -- Whitespace
+  nbsp = "␣",
+  tab = "→ ",
+  trail = "␠",
+  -- SpecialKey
+  conceal = "⬚",
+  extends = "…",
+  precedes = "…",
+}
+vim.schedule(function() -- Defer execution after init is done (to aviod overrides during colorscheme selection)
+  -- Highlight nonprinting characters
+  vim.cmd("highlight! link Whitespace WarningMsg")
+  vim.cmd("highlight! link SpecialKey ErrorMsg")
+end)
