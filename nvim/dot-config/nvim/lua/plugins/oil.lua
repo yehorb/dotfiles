a694e7670f227a3bf2fd0eee75fe57211c701b44
Yehor Borkov <yehor.borkov@gmail.com>
2026-06-08 17:16:33 +0300

    feat(nvim): set friendly oil options

--- nvim/dot-config/nvim/lua/plugins/oil.lua
+++ nvim/dot-config/nvim/lua/plugins/oil.lua
@@ -3,7 +3,18 @@ return {
     "stevearc/oil.nvim",
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
+    opts = {
+      keymaps = {
+        ["<C-h>"] = false,
+        ["<C-j>"] = false,
+        ["<C-k>"] = false,
+        ["<C-l>"] = false,
+        ["<C-r>"] = "actions.refresh",
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    },
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
     keys = {
