e0a5c916307fe5ee5e40d5bbabc6dc8dfb4f53e0
Yehor Borkov <yehor.borkov@gmail.com>
2026-06-22 13:31:39 +0300

    feat(nvim): update pyrefly settings

--- nvim/dot-config/nvim/pack/user/opt/python/plugin/lsp.lua
+++ nvim/dot-config/nvim/pack/user/opt/python/plugin/lsp.lua
@@ -1,1 +1,12 @@
+vim.lsp.config("pyrefly", {
+  cmd = { "uv", "tool", "run", "pyrefly", "lsp" },
+  init_options = {
+    pyrefly = {
+      typeCheckingMode = "auto",
+      analysis = {
+        showHoverGoToLinks = false,
+      },
+    },
+  },
+})
 vim.lsp.enable("pyrefly")
