12a493ad2c10feb925ba0d27f6d15b7434bbfe70
Yehor Borkov <yehor.borkov@gmail.com>
2026-09-24 12:26:53 +0300

    feat(nvim): restore the default behavior of c-n and c-p

---
+++ nvim/dot-config/nvim/lua/plugins/blink-ctrl-n-fallback.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
+      },
+    },
+  },
+}
